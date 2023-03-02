d165c452a675ced7f3233f4af4dd7db2682f1444
Santiago Suárez <santiago.suarez@axcelsoftware.com>
2023-03-02 13:53:11 -0500

    chore: init

---
+++ app/config/program.ts
@@ -0,0 +1,28 @@
+import { Command } from "commander";
+import path from "path";
+import { sticker } from "@poppinss/cliui";
+
+const program = new Command();
+
+program
+  .alias("cmv")
+  .name("cmv")
+  .description("CM Validations CLI")
+  .version("0.1.0");
+
+program
+  .command("stringify-json")
+  .description("Stringify the JSON")
+  .argument("<route>", "route of json")
+  .argument("<id_sick>", "id of sick")
+  .argument("<id_error>", "id of error")
+  .action((route, id_sick, id_error) => {
+    const _JSON = require(path.join(process.cwd(), route));
+
+    sticker()
+      .add(`('${JSON.stringify(_JSON)}',${id_sick},"${id_error}")`)
+
+      .render();
+  });
+
+export default program;
---
+++ app/index.ts
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+import program from "./config/program";
+
+program.parse();
